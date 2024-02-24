7ae778935ccd53f469013cf98c02b47b26f2a372
DakotaPatterson <158085432+DakotaPatterson@users.noreply.github.com>
2024-02-23 20:16:03 -0500

    Added the generatePassword function

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+//Set global arrays for the generatePassword function
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var specialCharacters = ["!","$","#","&","*",".","%","~","|","{","}","`","_","^","[","]","@","?",">","<","+","=",":",";","/","-"];
 var lowerCaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var Criteria = "";
+var generatedPassword = "";
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -25,31 +28,63 @@ function generatePassword() {
   var length = window.prompt ("How long would you like your password to be? It can be between 8 and 128 characters!");
 
   if (length < 8 || length > 128){
+
     window.alert("The length does not match the criteria!");
-    var length = window.prompt ("The length of the password must be between 8 and 128 characters:")
+
+    length = window.prompt ("The length of the password must be between 8 and 128 characters:")
+
   };
 
-  var numbers = window.confirm ("Would you like to include numbers in your password?");
+  var numbersSelected = window.confirm ("Would you like to include numbers in your password?");
 
-  var specialCharacters = window.confirm ("Would you like to include special characters in your password?");
+  var specialCharactersSelected = window.confirm ("Would you like to include special characters in your password?");
 
-  var lowerCaseLetters = window.confirm ("Would you like to include lower case letters in your password?");
+  var lowerCaseLettersSelected = window.confirm ("Would you like to include lower case letters in your password?");
 
-  var upperCaseLetters = window.confirm ("Would you like to include upper case letters in your password?");
+  var upperCaseLettersSelected = window.confirm ("Would you like to include upper case letters in your password?");
 
 
-  if (numbers === false && specialCharacters === false && lowerCaseLetters === false && upperCaseLetters === false){
+  if (numbersSelected === false && specialCharactersSelected === false && lowerCaseLettersSelected === false && upperCaseLetters === false){
+
       window.alert("The password needs to include at least one of the following: Upper case letters, Lower case letters, Special characters or Numbers!");
-      
-      var numbers = window.confirm ("Would you like to include numbers in your password?");
 
-      var specialCharacters = window.confirm ("Would you like to include special characters in your password?");
+      numbersSelected = window.confirm ("Would you like to include numbers in your password?");
 
-      var lowerCaseLetters = window.confirm ("Would you like to include lower case letters in your password?");
+      specialCharactersSelected = window.confirm ("Would you like to include special characters in your password?");
 
-      var upperCaseLetters = window.confirm ("Would you like to include upper case letters in your password?");
+      lowerCaseLettersSelected = window.confirm ("Would you like to include lower case letters in your password?");
+
+      upperCaseLettersSelected = window.confirm ("Would you like to include upper case letters in your password?");
 
   };
 
+  if (numbersSelected){
+    Criteria += numbers.join('');
+  };
+
+  if (specialCharactersSelected) {
+    Criteria += specialCharacters.join('');
+  };
+
+  if (lowerCaseLettersSelected) {
+    Criteria += lowerCaseLetters.join('');
+  };
+
+  if (upperCaseLettersSelected) {
+    Criteria += upperCaseLetters.join('');
+  };
+
+  for (let i = 0; i < length; i++) {
+
+    var index = Math.floor(Math.random() * Criteria.length); 
+
+    generatedPassword += Criteria[index];
+
+    console.log(generatedPassword);
+
+  };
+  
+  return generatedPassword;
+  
 };
 
